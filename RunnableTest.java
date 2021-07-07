8269021a62d6fe1092996c1c5c81a4a2f7228bb7
karthikkuruva <86503142+karthikkuruva@users.noreply.github.com>
2021-07-07 06:44:08 +0530

    Assignments on Threads

---
+++ RunnableTest.java
@@ -0,0 +1,17 @@
+package com.intellect.threads;
+
+public class RunnableTest {
+
+	public static void main(String[] args) {
+		System.out.println("Main thread is- " + Thread.currentThread().getName());
+		Thread t1 = new Thread(new RunnableTest().new RunnableImpl());
+		t1.start();
+	}
+
+	private class RunnableImpl implements Runnable {
+
+		public void run() {
+			System.out.println(Thread.currentThread().getName() + ", executing run() method!");
+		}
+	}
+}
